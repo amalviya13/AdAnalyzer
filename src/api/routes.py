502fe16c116936ddf3176007fcbd6665fad29fc8
Arnav  Malviya <arnavmalviya@Arnav-MacBook.local>
2020-08-28 15:38:44 -0700

    API planning

---
+++ src/api/routes.py
@@ -0,0 +1,15 @@
+# POST /image/upload - Uploads all images based on csv sent
+# POST /image/resize - Resize image 200x200
+# POST /image/calculate - Runs all calculations on image set
+# GET /image/dominant - Dominant color in the image
+# GET /image/pallette/{numPallete} - Top x colors in the image
+# GET /image/weighted - Get all colors in image with # appearances
+# GET /image/percentage/{percent} - Top x percentage of colors
+# GET /image/warmcool - Is the image warm or cool
+
+# POST /set/upload - Uploads all images based on csv sent
+# POST /set/resize - Resize all images 200x200
+# POST /set/calculate - Runs all calculations on image set
+# GET /set/dominant - Dominant color overall
+# GET /set/colorset - Gets all colors in image with # appearances
+# GET /set/unique - Average number of unique colors
