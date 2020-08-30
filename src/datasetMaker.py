347f24f6b439cd5962f1b70c969197da39fe3102
aamarnath2 <aamarnat@usc.edu>
2020-08-30 14:18:29 -0700

    script to make a fake dataset

---
+++ src/datasetMaker.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+import csv
+import os
+import random
+
+
+def makeFakeDataset(filePath):
+	with open('adDataset.csv', 'w', newline='') as csvFile:
+		writer = csv.writer(csvFile)
+		writer.writerow(["FileName", "CTR"])
+		for filename in os.listdir(filePath):
+			writer.writerow([os.path.join(filePath, filename), random.uniform(0, 0.6)])
+
+if __name__ == '__main__':
+	parser = ArgumentParser()
+    parser.add_argument('--directoryPath', default='N/A')
+    args = parser.parse_args()
+	makeFakeDataset(args.directoryPath)
