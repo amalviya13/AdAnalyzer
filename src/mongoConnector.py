31b289b22ae6f731e418a215b9f44d870ac04b8e
Arnav  Malviya <arnavmalviya@Arnav-MacBook.local>
2020-09-17 00:08:59 -0700

    initial commit

--- src/mongoConnector.py
+++ src/mongoConnector.py
@@ -12,35 +12,62 @@ app = Flask(__name__)
 client = pymongo.MongoClient(connection_url) 
 companiesDB = client["companies"]
 
+#Upload new image   
 @app.route('/image/upload', methods=['POST'])
-def uploadImage():
+def newImage():
     data = json.loads(request.data)
     route = data['route']
     ctr = data['ctr']
+    setName = data['set']
     warm_cool = warm_or_cool(data['route'])
     top_color = weightedColors(data['route'])
-    status = companiesDB.nike.insert_one({'image_route' : route, 'warm_or_cool' : warm_cool, 'top_colors' : top_color, 'ctr' : ctr})
+    status = companiesDB.nike.insert_one({'image_route' : route, 'set' : setName, 'warm_or_cool' : warm_cool, 'top_colors' : top_color, 'ctr' : ctr})
     return "Uploaded Image"
 
+#Upload new image set 
 @app.route('/image/set/upload', methods=['POST'])
-def uploadImageSet():
+def newImageSet():
     data = json.loads(request.data)
     fileDict = getCSVData(data['route'])
+    setName = data['set']
     counter = 0
     myObj = []
     for key, value in fileDict.items():  
-        if counter == 123:
-            break
         warm_cool = warm_or_cool(key)
         top_color = weightedColors(key)
-        counter = counter + 1
-        myObj.append({'image_route' : key, 'warm_or_cool' : warm_cool, 'top_colors' : top_color, 'ctr' : value})
+        myObj.append({'image_route' : key, 'set' : setName, 'warm_or_cool' : warm_cool, 'top_colors' : top_color, 'ctr' : value})
 
     companiesDB.nike.insert_many(myObj)
 
     color_set = get_color_set(data['route'])
-    status = companiesDB.nike.insert_one({'set_route' : route, 'color_set' : color_set, 'num_images' : counter})
+    status = companiesDB.nike.insert_one({'set_route' : route, 'set' : setName, 'color_set' : color_set, 'num_images' : counter})
     return "Uploaded Set"
+
+# #Adding a new set to existing set
+# @app.route('/image/set/add', methods=['POST'])
+# def updateImageSet():
+
+# #Add new image to a set
+# @app.route('/image/add', methods=['POST'])
+# def addImageToSet():
+
+#Delete a set and all corresponding images
+@app.route('/image/set/delete', methods=['DELETE'])
+def deleteImageSet():
+    data = json.loads(request.data)
+    setName = data['set']
+
+    # for key, value in fileDict.items():  
+    #     companiesDB.nike.delete_one({'image_route' : key})
+    status = companiesDB.nike.delete_many({'set' : setName})
+    return "Deleted Set"
+
+#Delete an image 
+@app.route('/image/delete', methods=['DELETE'])
+def deleteImage():
+    data = json.loads(request.data)
+    companiesDB.nike.delete_one({'image_route' : data['route']})
+    return "Deleted Image"
 
 if __name__ == '__main__': 
     app.run(debug=True) 
